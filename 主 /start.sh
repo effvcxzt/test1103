d259b75ef015380c0d734683b55f697bc1a2c488
Rita W. Smith <105619204+effvcxzt@users.noreply.github.com>
2022-11-03 13:44:50 +0800

    Update and rename main/start.sh to 主 /start.sh

--- 主 /start.sh
+++ 主 /start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-90e9ee398fd8439faa487a8078d86472}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
